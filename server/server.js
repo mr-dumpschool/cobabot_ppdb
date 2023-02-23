3b1fbae9b92435f8df28bfa259acacb26ac222fd
unknown <arifbudisantoso@smkn1gombong.sch.id>
2023-02-23 15:23:20 +0700

    first commit

---
+++ server/server.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
+import fs from 'fs';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Halo, Saya adalah BOT...',
+  });
+});
+
+app.post('/', async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+    let backgroundInfo = '';
+    try {
+      backgroundInfo = fs.readFileSync('info.txt', 'utf-8');
+    } catch (err) {
+      console.error(err);
+      backgroundInfo = 'Default background information';
+    }
+    const fullPrompt = `${backgroundInfo}\n\n${prompt}`;
+
+    const response = await openai.createCompletion({
+      model: 'text-davinci-002',
+      prompt: fullPrompt,
+      temperature: 0.5,
+      max_tokens: 1024,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'));
