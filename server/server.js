c1c4757980cc8e1ea77b61e035eb359543440e0e
mr-dumpschool <73808308+mr-dumpschool@users.noreply.github.com>
2023-02-24 08:35:26 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -53,4 +53,5 @@ app.post('/', async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`AI server started on http://localhost:${PORT}`));
