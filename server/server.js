322da04e0f7f564874d35726b21f8a2dc8b3c9b4
mr-dumpschool <73808308+mr-dumpschool@users.noreply.github.com>
2023-02-27 11:49:24 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const { Configuration, OpenAIApi } = require('openai');
 const fs = require('fs');
-const dotenv = require('dotenv');
-dotenv.config();
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
